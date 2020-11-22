12a707529133aa9e995a445455feaaab83f3f938
juansrodz <juansrodz@gmail.com>
2020-11-21 22:14:47 -0500

    CORS

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
-// module.exports = {
-//   "devServer": {
-//     "proxy": {
-//       "/api": {
-//         "target": "https://grey-matter-backend.herokuapp.com/api",
-//         "pathRewrite": {
-//           "^/api": ""
-//         },
-//         "changeOrigin": true,
-//         "secure": false
-//       }
-//     }
-//   },
-//   "transpileDependencies": [
-//     "vuetify"
-//   ]
-// }
+module.exports = {
+  "devServer": {
+    "proxy": {
+      "/api": {
+        "target": "https://grey-matter-backend.herokuapp.com/api",
+        "pathRewrite": {
+          "^/api": ""
+        },
+        "changeOrigin": true,
+        "secure": false
+      }
+    }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
